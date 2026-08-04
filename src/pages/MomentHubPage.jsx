5a2555369d4ff17612ec684556f0b4c4d1c0a61a
Joey Dooling <Mewto36010@gmail.com>
2026-08-03 23:36:01 -0500

    Swap swipe arrows back: Moment left, Journey right

--- src/pages/MomentHubPage.jsx
+++ src/pages/MomentHubPage.jsx
@@ -7,7 +7,7 @@ const easeSoft = [0.16, 1, 0.3, 1]
 
 export default function MomentHubPage() {
   return (
-    <SwipeShell leftTo="/" className="page">
+    <SwipeShell rightTo="/" className="page">
       <motion.section
         initial={{ opacity: 0, y: 12 }}
         animate={{ opacity: 1, y: 0 }}
@@ -35,7 +35,7 @@ export default function MomentHubPage() {
         </motion.div>
       ))}
 
-      <p className="home-quiet-link">Swipe left, or use the menu, to go back home.</p>
+      <p className="home-quiet-link">Swipe right, or use the menu, to go back home.</p>
     </SwipeShell>
   )
 }
